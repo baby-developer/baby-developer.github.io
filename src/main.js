adca410a3fdb935f28b8002011c45d0ac3e59ecc
baby-developer <queeh316@hanmail.net>
2023-10-10 09:51:13 +0900

    아래로 스크롤 시 Home 섹션의 컨텐츠가 투명하게 처리되도록 함

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@
 // 페이지 아래로 스크롤 시 Header에 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-window.addEventListener('scroll', () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// 페이지 아래로 스크롤 시 Home 섹션을 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
