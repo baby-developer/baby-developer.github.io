b7e028218737d32d5b905983ce018029c9e4be42
baby-developer <queeh316@hanmail.net>
2023-10-10 10:17:59 +0900

    아래로 스크롤 시 Arrow up 버튼을 보이도록 함

--- src/main.js
+++ src/main.js
@@ -17,3 +17,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// 페이지 아래로 스크롤 시 Arrow up 버튼을 보이도록 함
+const arrowUpButton = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUpButton.style.opacity = 1;
+  } else {
+    arrowUpButton.style.opacity = 0;
+  }
+});
